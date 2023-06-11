9902fbc68aee97063f7a25eb49c432f4f0feee94
02utkarsh <utkarsh7424sharma@gmail.com>
2023-06-11 19:25:32 +0530

    this is the first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        tmv:'rgb(4,3,45)',
+        newa:'rgb(24,28,46)'
+      },
+    },
+  },
+  
+  plugins: [],
+}
+
